b49ff879d5f31f696b091c49259ffb76719a8a94
jaafardialloo <jaafardialloo@gmail.com>
2020-08-21 21:11:42 +0100

    Exchanger

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+ function Header() {
+
+  return (
+    <div  className="header">
+         <h1 className="currancy">Welcome to your currency Exchanger</h1>
+    </div>
+  )
+}
+export default Header;
---
+++ src/logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import logo from '../src/Ouguya.jpg'
+
+ function Logo() {
+
+  return (
+    <div  className="logo">
+         <img src={logo} />
+    </div>
+  )
+}
+export default Logo;
